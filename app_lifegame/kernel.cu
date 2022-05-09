d8af827041422c17eeb53cc1fec51a11f0f0d238
srkata <srkata@stud.uni-obuda.hu>
2022-05-09 23:58:47 +0200

    Fix position calculation

--- app_lifegame/kernel.cu
+++ app_lifegame/kernel.cu
@@ -89,17 +89,21 @@ void setOneCellArray(int* cells0, int pos,const int n,const int m, int* cells_ne
 		//count
 		if (pos >= m)
 		{
-			if ((pos - m - 1) % m >= 0) { count += cells0[pos - m - 1]; }
+			if ((pos - m - 1) % m != m-1) 
+				{ count += cells0[pos - m - 1]; }
 			count += cells0[pos - m];
-			if ((pos - m + 1) % m < m) { count += cells0[pos - m + 1]; }
+			if ((pos - m + 1) % m != 0) 
+				{ count += cells0[pos - m + 1]; }
 		}
-		if ((pos - 1) % m >= 0) { count += cells0[pos - 1]; }
-		if ((pos + 1) % m < m) { count += cells0[pos + 1]; }
+		if ((pos - 1) % m != m-1) { count += cells0[pos - 1]; }
+		if ((pos + 1) % m != 0) { count += cells0[pos + 1]; }
 		if (pos + m < m * n)
 		{
-			if ((pos + m - 1) % m >= 0) { count += cells0[pos + m - 1]; }
+			if ((pos + m - 1) % m != m - 1)
+				{ count += cells0[pos + m - 1]; }
 			count += cells0[pos + m];
-			if ((pos + m + 1) % m < m) { count += cells0[pos + m + 1]; }
+			if ((pos + m + 1) % m != 0)
+				{ count += cells0[pos + m + 1]; }
 		}
 		//end count
 
@@ -114,63 +118,10 @@ void setOneCellArray(int* cells0, int pos,const int n,const int m, int* cells_ne
 	}
 }
 
-__device__ int dev_n, dev_m;
-
-__global__
-void dev_generateTestfile(int* cells)
-{
-	int pos = blockIdx.x*blockDim.y* blockDim.x +threadIdx.x * blockDim.y + threadIdx.y;
-	cells[pos] = pos;
-}
-
-__global__ void dev_setOneCell(int* dev_cells0, int* dev_cells_next)
-{
-	/*__shared__ int* dev_cells_next;
-	cudaMalloc((void**)&dev_cells_next, (dev_n * dev_m) * sizeof(int));*/
-	int pos = blockIdx.x * blockDim.y * blockDim.x + threadIdx.x * blockDim.y + threadIdx.y;
-	if (dev_cells0[pos] != 2)
-	{
-		int count = 0;
-
-		//count
-		if (pos >= dev_m)
-		{
-			if ((pos - dev_m - 1) % dev_m >= 0) { count += dev_cells0[pos - dev_m - 1]; }
-			count += dev_cells0[pos - dev_m];
-			if ((pos - dev_m + 1) % dev_m < dev_m) { count += dev_cells0[pos - dev_m + 1]; }
-		}
-		if ((pos - 1)%dev_m >= 0) { count += dev_cells0[pos-1]; }
-		if ((pos + 1)%dev_m < dev_m) { count += dev_cells0[pos+1]; }
-		if (pos + dev_m < dev_m * dev_n)
-		{
-			if ((pos + dev_m - 1) % dev_m >= 0) { count += dev_cells0[pos + dev_m - 1]; }
-			count += dev_cells0[pos + dev_m];
-			if ((pos + dev_m + 1) % dev_m < dev_m) { count += dev_cells0[pos + dev_m + 1]; }
-		}
-		//end count
-
-		if ((count == 3) || ((dev_cells0[pos] == 1) && (count == 2)))
-		{
-			dev_cells_next[pos] = 1;
-		}
-		else
-		{
-			dev_cells_next[pos] = 0;
-		}
-	}
-	/*__syncthreads();
-	if (pos == 0)
-	{
-		int* temp = dev_cells0;
-		dev_cells0 = dev_cells_next;
-		dev_cells_next = temp;
-	}
-	cudaFree(dev_cells_next);*/
-}
 
 void runCPU(int nrOfGeneration)
 {
-	char infile[100] = "testfiles\\C7x5oGlider.txt";
+	char infile[100] = "testfiles\\F3x3o.txt";
 	std::ifstream in(infile);
 	int n, m;
 	in >> m;
@@ -197,19 +148,77 @@ void runCPU(int nrOfGeneration)
 	free(cellsCPU0);
 }
 
-int main()
+__device__ int dev_n, dev_m, dev_nrGeneration;
+
+__global__
+void dev_generateTestfile(int* cells)
 {
-	//runCPU(8);
+	int pos = blockIdx.x*blockDim.y* blockDim.x +threadIdx.x * blockDim.y + threadIdx.y;
+	cells[pos] = pos;
+}
 
+__global__ void dev_setOneCell(int* dev_cells0, int* dev_cells_next)
+{
+	__shared__ int dev_gen;
+	dev_gen = dev_nrGeneration;
+	int pos = blockIdx.x * blockDim.y * blockDim.x + threadIdx.x * blockDim.y + threadIdx.y;
+	while (dev_gen > 0)
+	{
+		if (dev_cells0[pos] != 2)
+		{
+			int count = 0;
+
+			//count
+			if (pos >= dev_m)
+			{
+				if ((pos - dev_m - 1) % dev_m != dev_m -1) { count += dev_cells0[pos - dev_m - 1]; }
+				count += dev_cells0[pos - dev_m];
+				if ((pos - dev_m + 1) % dev_m !=0) { count += dev_cells0[pos - dev_m + 1]; }
+			}
+			if ((pos - 1) % dev_m != dev_m - 1) { count += dev_cells0[pos - 1]; }
+			if ((pos + 1) % dev_m != 0) { count += dev_cells0[pos + 1]; }
+			if (pos + dev_m < dev_m * dev_n)
+			{
+				if ((pos + dev_m - 1) % dev_m != dev_m - 1) { count += dev_cells0[pos + dev_m - 1]; }
+				count += dev_cells0[pos + dev_m];
+				if ((pos + dev_m + 1) % dev_m != 0) { count += dev_cells0[pos + dev_m + 1]; }
+			}
+			//end count
+
+			if ((count == 3) || ((dev_cells0[pos] == 1) && (count == 2)))
+			{
+				dev_cells_next[pos] = 1;
+			}
+			else
+			{
+				dev_cells_next[pos] = 0;
+			}
+		}
+		__syncthreads();
+		int temp = dev_cells0[pos];
+		dev_cells0[pos] = dev_cells_next[pos];
+		dev_cells_next[pos] = temp;
+		__syncthreads();
+		if (pos == 0)
+		{
+			dev_gen--;
+		}
+		__syncthreads();
+	}
+	
+}
+
+void runGPU(int nrOfGeneration)
+{
 	int n, m;
-
-	char infile[100] = "testfiles\\A5x5oBlinker.txt";
+	char infile[100] = "testfiles\\F3x3o.txt";
 	std::ifstream in(infile);
 	in >> m;
 	in >> n;
 	cudaMemcpyToSymbol(dev_m, &m, sizeof(int));
 	cudaMemcpyToSymbol(dev_n, &n, sizeof(int));
-	std::cout<<n<<" "<<m<<std::endl;
+	cudaMemcpyToSymbol(dev_nrGeneration, &nrOfGeneration, sizeof(int));
+	std::cout << n << " " << m << std::endl;
 	int* cells00 = (int*)malloc((n * m) * sizeof(int));
 	readtestfile(in, cells00);
 	in.close();
@@ -217,25 +226,31 @@ int main()
 
 	int blocksizex = 32, blocksizey = 30;
 	int blockCount = (n * m) / (blocksizex * blocksizey) + 1;
-	
+
 	int* dev_cells0;
 	cudaMalloc((void**)&dev_cells0, (n * m) * sizeof(int));
 	cudaMemcpy(dev_cells0, cells00, (n * m) * sizeof(int), cudaMemcpyHostToDevice);
 
-	///try
 	int* dev_cells_next;
 	cudaMalloc((void**)&dev_cells_next, (n * m) * sizeof(int));
 
 	dim3 blocksize(blocksizex, blocksizey);
-	dev_setOneCell <<<blockCount, blocksize >>> (dev_cells0, dev_cells_next);
-	cudaMemcpy(cells00, dev_cells_next, (n * m) * sizeof(int), cudaMemcpyDeviceToHost);
-	writeToConsole(n,m,cells00);
-	//std::cout << cudaGetErrorString(err);
-	
+	dev_setOneCell <<<blockCount, blocksize>>> (dev_cells0, dev_cells_next);
+	cudaMemcpy(cells00, dev_cells0, (n * m) * sizeof(int), cudaMemcpyDeviceToHost);
+	writeToConsole(n, m, cells00);
+
 
 	free(cells00);
 	cudaFree(dev_cells0);
 	cudaFree(dev_cells_next);
+}
+
+int main()
+{
+	runCPU(1);
+	std::cout<<"\nGPU\n";
+	runGPU(1);
+	
     return 0;
 }
 
